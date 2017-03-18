d7ee5d661435143d3eb62731147cf43102d58ec6
studiotang <studiotang.cn@gmail.com>
2017-03-18 23:36:37 +0800

    Add ultra pull to refresh layout

---
+++ app/src/main/java/com/free/blog/ui/BaseRefreshActivity.java
@@ -0,0 +1,10 @@
+package com.free.blog.ui;
+
+import com.free.blog.ui.activity.BaseActivity;
+
+/**
+ * @author studiotang on 17/3/18
+ */
+public class BaseRefreshActivity extends BaseActivity{
+
+}
