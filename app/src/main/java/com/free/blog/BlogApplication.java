4f13c4b0fac2f325ca8d63dccaabb794d0b403ce
studiotang <studiotang.cn@gmail.com>
2017-03-22 23:07:08 +0800

    Optimize application

--- app/src/main/java/com/free/blog/BlogApplication.java
+++ app/src/main/java/com/free/blog/BlogApplication.java
@@ -14,94 +14,75 @@ import java.util.List;
 
 /**
  * 应用Application类
- * 
+ *
  * @author tangqi
  * @since 2015年7月8日下午11:47:10
  */
 
 public class BlogApplication extends Application {
 
-	private static BlogApplication mInstance;
-	private List<Activity> mActivities = new ArrayList<Activity>();
+    private static BlogApplication mInstance;
+    private List<Activity> mActivities = new ArrayList<Activity>();
 
-	// 单例模式中获取唯一的ExitApplication 实例
-	public static BlogApplication getInstance() {
-		return mInstance;
+    public static BlogApplication getInstance() {
+        return mInstance;
 
-	}
+    }
 
-	public static Context getContext() {
-		return mInstance;
-	}
+    public static Context getContext() {
+        return mInstance;
+    }
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		mInstance = this;
-		init();
-	}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        init();
+    }
 
-	private void init() {
-		initImageLoader();
+    private void init() {
+        initCrashReport();
+    }
 
-		initCrashReport();
-	}
+    @SuppressWarnings("unused")
+    private void initCrashHandler() {
+        CrashHandler crashHandler = CrashHandler.getInstance();
+        crashHandler.init(this);
+        Thread.currentThread().setUncaughtExceptionHandler(crashHandler);
+    }
 
-	/**
-	 * 初始化ImageLoader
-	 */
-	private void initImageLoader() {
+    /**
+     * 初始化崩溃上传(腾讯BUGLY)
+     */
+    private void initCrashReport() {
+        CrashReport.initCrashReport(this, "900007710", false);
+    }
 
-	}
+    /**
+     * 重载系统获取缓存目录
+     */
+    @Override
+    public File getCacheDir() {
+        return new File(CacheManager.getAppCachePath(this));
+    }
 
-	/**
-	 * 初始化CrashHandler(保存在本地)
-	 */
-	@SuppressWarnings("unused")
-	private void initCrashHandler() {
-		CrashHandler crashHandler = CrashHandler.getInstance();
-		crashHandler.init(this);
-		Thread.currentThread().setUncaughtExceptionHandler(crashHandler);
-	}
+    @Override
+    public File getDatabasePath(String name) {
+        return new File(CacheManager.getAppDatabasePath(this));
+    }
 
-	/**
-	 * 初始化崩溃上传(腾讯BUGLY)
-	 */
-	private void initCrashReport() {
-		CrashReport.initCrashReport(this, "900007710", false);
-	}
+    public void addActivity(Activity activity) {
+        mActivities.add(activity);
+    }
 
-	/**
-	 * 重载系统获取缓存目录
-	 */
-	@Override
-	public File getCacheDir() {
-		return new File(CacheManager.getAppCachePath(this));
-	}
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
 
-	@Override
-	public File getDatabasePath(String name) {
-		return new File(CacheManager.getAppDatabasePath(this));
-	}
+        for (Activity activity : mActivities) {
+            activity.finish();
+        }
 
-	/**
-	 * 把Activity加入历史堆栈
-	 */
-	public void addActivity(Activity activity) {
-		mActivities.add(activity);
-	}
-
-	/**
-	 * 结束
-	 */
-	@Override
-	public void onTerminate() {
-		super.onTerminate();
-
-		for (Activity activity : mActivities) {
-			activity.finish();
-		}
-
-		System.exit(0);
-	}
+        System.exit(0);
+    }
 }
